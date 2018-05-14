56c6140a5c652e0adf425766f5084d27a2cef88a
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-05-14 13:42:09 +0200

    http error docs

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import SocketComponent from './component.socket';
 import Page from './component.page';
 import Socket from './socket';
 import Storage from './storage';
+import ErrorPage from './page.error';
 
 export {
     Application,
@@ -12,4 +13,5 @@ export {
     Page,
     Socket,
     Storage,
+    ErrorPage,
 };
