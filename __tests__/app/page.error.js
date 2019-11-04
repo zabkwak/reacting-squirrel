c9c2a996d8491ddb01946c358b09d1f4d8c0cf7e
Lukáš Mačuda <lukas.macuda@gmail.com>
2019-11-04 14:12:07 +0100

    custom error page example

--- __tests__/app/page.error.js
+++ __tests__/app/page.error.js
@@ -1,9 +1,17 @@
+import React from 'react';
+
 import { ErrorPage as Base } from '../../src/app';
 
 export default class ErrorPage extends Base {
 
-	componentDidMount() {
-		super.componentDidMount();
-		console.log('custom error');
+	render() {
+		const { message } = this.props.error;
+		return (
+			<div className="error">
+				<h2>{message}</h2>
+				{this.renderStack()}
+				<h3>Custom error text</h3>
+			</div>
+		);
 	}
 }
