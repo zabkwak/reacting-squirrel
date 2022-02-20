1a915bef6c514227ddef4c3dd27ad088ef44ba43
zabkwak <lukas.macuda@gmail.com>
2022-02-20 08:02:25 +0100

    updated dependencies

--- __tests__/cases/app/components.js
+++ __tests__/cases/app/components.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import chai, { expect } from 'chai';
 import Enzyme, { mount, shallow } from 'enzyme';
